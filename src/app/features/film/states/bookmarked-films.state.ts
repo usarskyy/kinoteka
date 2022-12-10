31c60b8c8d7158068d20af3bcfe4c0c0dae088b2
Volodymyr Usarskyy <usarskyy@gmail.com>
2022-12-10 18:41:39 +0100

    bookmarked-films.state: removed unnecessary parent state class

--- src/app/features/film/states/bookmarked-films.state.ts
+++ src/app/features/film/states/bookmarked-films.state.ts
@@ -1,28 +1,52 @@
 import { Injectable } from '@angular/core';
 import { BookmarkedMediaDictionary } from '@core/interfaces';
-import { NgxState, ObjectState } from 'ngx-base-state';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { BookmarkEnum } from '../../bookmark';
 
-@NgxState()
 @Injectable({
     providedIn: 'root'
 })
-export class BookmarkedFilmsState extends ObjectState<BookmarkedMediaDictionary> {
+export class BookmarkedFilmsState {
+
+    private readonly state$ = new BehaviorSubject<BookmarkedMediaDictionary | null>(null);
+
+    ////// NOT REQUIRED, ADDED FOR BACKWARD COMPATIBILITY -> START ->
+    public get data$(): Observable<BookmarkedMediaDictionary | null> {
+      return this.state$;
+    }
+
+    public get data(): BookmarkedMediaDictionary | null {
+      return this.state$.value;
+    }
+
+    public set(value: BookmarkedMediaDictionary): void {
+      return this.state$.next(value);
+    }
+    ////// -> END
+
     public add(kinopoiskId: string, bookmark: BookmarkEnum): void {
+        debugger;
+
         let bookmarks = this.getBookmarks(kinopoiskId);
         bookmarks = [...bookmarks, bookmark];
 
-        this.updateWithPartial({ [kinopoiskId]: bookmarks });
+        const newState = {...this.state$.value, ...{ [kinopoiskId]: bookmarks }};
+
+        this.state$.next(newState);
     }
 
     public remove(kinopoiskId: string, bookmark: BookmarkEnum): void {
+        debugger;
+
         let bookmarks = this.getBookmarks(kinopoiskId);
         bookmarks = bookmarks.filter((currBookmark) => (currBookmark !== bookmark));
 
-        this.updateWithPartial({ [kinopoiskId]: bookmarks });
+        const newState = {...this.state$.value, ...{ [kinopoiskId]: bookmarks }};
+
+        this.state$.next(newState);
     }
 
     private getBookmarks(kinopoiskId: string): BookmarkEnum[] {
-        return this.data?.[kinopoiskId] ?? [];
+        return this.state$.value?.[kinopoiskId] ?? [];
     }
 }
